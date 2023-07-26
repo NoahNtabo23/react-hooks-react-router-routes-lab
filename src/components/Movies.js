5d50da34f49dbb29e93b9f470ed27c1d38cb33f1
Noah <noah.ntabo@student.moringaschool.com>
2023-07-27 00:17:31 +0300

    Completed assignment

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,26 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  return(
+    <div>
+      <div>
+        <h1>Movies Page</h1>
+      </div>
+      <div>
+        {movies.map((movie,index)=>(
+          <div key={index}>
+            <h2>{movie.title}</h2>
+            <h3>{movie.time}</h3>
+            <ul>
+              {movie.genres.map((genres,i)=>(
+                <li key={i}>{genres}</li>
+              ))}
+            </ul>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
 }
 
 export default Movies;
